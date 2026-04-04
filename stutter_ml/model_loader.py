bbf411cf2a29bbd2622100a37da8624f2997bed0
Nik848 <nik84810@gmail.com>
2026-04-04 15:30:05 +0530

    cloning

--- stutter_ml/model_loader.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-
-def load_model():
-    classifier = pipeline(
-        "audio-classification",
-        model="HareemFatima/distilhubert-finetuned-stutterdetection"
-    )
-    return classifier
